1ba5815e39111e453c3e0210f535cdda6b22ba87
MIN SEONG KIM <kyrok67@naver.com>
2021-04-28 23:45:39 +0900

    구현

---
+++ 구현/이진수(백준3460).swift
@@ -0,0 +1,26 @@
+// 이진수 - 구현
+// 예제 입력            예제 출력
+// 1                    0 2 3
+// 13
+
+//let n = Int(readLine()!)!
+//var answerArr: [String] = []
+//
+//for _ in 1...n {
+//    var testCase = Int(readLine()!)!
+//    var cnt = 0
+//    while testCase != 1 {
+//        if testCase % 2 == 1{
+//            answerArr.append(String(cnt))
+//        }
+//        testCase /= 2
+//        cnt += 1
+//    }
+//    answerArr.append(String(cnt))
+//    print(answerArr.joined(separator: " "))
+//    answerArr = []
+//}
+
+
+// 이진수 변환 내장함수 String(Int, radix:2) 사용해도됨
+
