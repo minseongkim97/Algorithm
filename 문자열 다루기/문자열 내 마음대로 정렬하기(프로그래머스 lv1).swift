8b1e7a2e35b35d92edc2e395167e0a55c2d6cbed
MIN SEONG KIM <kyrok67@naver.com>
2021-04-30 03:39:36 +0900

    문자열 다루기

--- 문자열 다루기/문자열 내 마음대로 정렬하기(프로그래머스 lv1).swift
+++ 문자열 다루기/문자열 내 마음대로 정렬하기(프로그래머스 lv1).swift
@@ -4,7 +4,7 @@
 // ["abce", "abcd", "cdx"] 2    ["abcd", "abce", "cdx"]
 
 func solution(_ strings:[String], _ n:Int) -> [String] {
-    let index = strings[0].index(strings[0].startIndex, offsetBy: n)
+    let index = strings[2].index(strings[2].startIndex, offsetBy: n)
 
     let result = strings.sorted(by : {(s1 : String, s2 : String) -> Bool in
         if s1[index] == s2[index] {
@@ -20,4 +20,13 @@ func solution(_ strings:[String], _ n:Int) -> [String] {
 
 print(solution(["sun", "bed", "car"], 1))
 
+// 다른 사람 풀이
+
+func solution(_ strings:[String], _ n:Int) -> [String] {
+    return strings.sorted{  Array($0)[n] == Array($1)[n] ? $0 < $1 :  Array($0)[n] < Array($1)[n] }
+}
+
+
 // 문자열 index와 Array 정렬에 대해 더 공부해봐야겠다.
+// strings[0]에서 0 대신 1,2 ,.. 원소의 개수-1까지 들어가도 상관없이 index는 1을 가르킬 것이다.
+// 삼항연산자 사용
